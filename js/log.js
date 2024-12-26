cc818785bd67aa0fe7ba948b69f825aeb691a0a9
Fiza <fiza@gmail.com>
2024-12-26 19:33:28 +0600

    first commit

---
+++ js/log.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const avatar = document.querySelector('.avatar');
+    avatar.addEventListener('click', function() {
+        alert('Avatar clicked!');
+    });
+});
